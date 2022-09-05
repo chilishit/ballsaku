af1660e22556a7c7b9f8dbfd6064456ad34e19b7
crustman69 <73906828+crustman69@users.noreply.github.com>
2022-09-04 20:53:29 -0500

    first and only commit
    bolas testiculos culo

---
+++ _maps/Eggman.asm
@@ -0,0 +1,60 @@
+Map_109c:
+Map_109c_0: 	dc.w Map_109c_1A-Map_109c
+Map_109c_2: 	dc.w Map_109c_39-Map_109c
+Map_109c_4: 	dc.w Map_109c_44-Map_109c
+Map_109c_6: 	dc.w Map_109c_4F-Map_109c
+Map_109c_8: 	dc.w Map_109c_5F-Map_109c
+Map_109c_A: 	dc.w Map_109c_6F-Map_109c
+Map_109c_C: 	dc.w Map_109c_7F-Map_109c
+Map_109c_E: 	dc.w Map_109c_8F-Map_109c
+Map_109c_10: 	dc.w Map_109c_A4-Map_109c
+Map_109c_12: 	dc.w Map_109c_AA-Map_109c
+Map_109c_14: 	dc.w Map_109c_B0-Map_109c
+Map_109c_16: 	dc.w Map_109c_B1-Map_109c
+Map_109c_18: 	dc.w Map_109c_BC-Map_109c
+Map_109c_1A: 	dc.b $6
+	dc.b $EC, $1, $0, $A, $E4
+	dc.b $EC, $5, $0, $C, $C
+	dc.b $FC, $E, $20, $10, $E4
+	dc.b $FC, $E, $20, $1C, $4
+	dc.b $14, $C, $20, $28, $EC
+	dc.b $14, $0, $20, $2C, $C
+Map_109c_39: 	dc.b $2
+	dc.b $E4, $4, $0, $0, $F4
+	dc.b $EC, $D, $0, $2, $EC
+Map_109c_44: 	dc.b $2
+	dc.b $E4, $4, $0, $0, $F4
+	dc.b $EC, $D, $0, $35, $EC
+Map_109c_4F: 	dc.b $3
+	dc.b $E4, $8, $0, $3D, $F4
+	dc.b $EC, $9, $0, $40, $EC
+	dc.b $EC, $5, $0, $46, $4
+Map_109c_5F: 	dc.b $3
+	dc.b $E4, $8, $0, $4A, $F4
+	dc.b $EC, $9, $0, $4D, $EC
+	dc.b $EC, $5, $0, $53, $4
+Map_109c_6F: 	dc.b $3
+	dc.b $E4, $8, $0, $57, $F4
+	dc.b $EC, $9, $0, $5A, $EC
+	dc.b $EC, $5, $0, $60, $4
+Map_109c_7F: 	dc.b $3
+	dc.b $E4, $4, $0, $64, $4
+	dc.b $E4, $4, $0, $0, $F4
+	dc.b $EC, $D, $0, $35, $EC
+Map_109c_8F: 	dc.b $4
+	dc.b $E4, $9, $0, $66, $F4
+	dc.b $E4, $8, $0, $57, $F4
+	dc.b $EC, $9, $0, $5A, $EC
+	dc.b $EC, $5, $0, $60, $4
+Map_109c_A4: 	dc.b $1
+	dc.b $4, $5, $0, $2D, $22
+Map_109c_AA: 	dc.b $1
+	dc.b $4, $5, $0, $31, $22
+Map_109c_B0: 	dc.b $0
+Map_109c_B1: 	dc.b $2
+	dc.b $0, $8, $1, $2A, $22
+	dc.b $8, $8, $11, $2A, $22
+Map_109c_BC: 	dc.b $2
+	dc.b $F8, $B, $1, $2D, $22
+	dc.b $0, $1, $1, $39, $3A
+	even
